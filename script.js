7dacc2df16e4269ca3423604012765068dcb90c9
Laszlo Abonyi <42050292+Necro1981@users.noreply.github.com>
2019-03-02 18:26:07 +1100

    Background Color Generator
    Project required to build Background color generator using DOM manipulation/event listeners make the page interactive. Also required a button which enables users to generate random gradient background colors.

--- script.js
+++ script.js
@@ -1,66 +1,39 @@
-const input = document.getElementById("userinput");
-const button = document.getElementById("enter");
-const list = document.getElementById("mylist");
-const item = document.getElementsByTagName("li");
-const btn = document.getElementsByClassName("delete");
-const header = document.querySelector("h1");
-//cycles through btn array and adds an event listener to each existing item
-//event listener triggers removeParent function
-for(let i=0; i < btn.length; i++){
-	btn[i].addEventListener("click",removeParent);
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const css = document.querySelector("h3");
+const body = document.getElementById("gradient");
+const random = document.getElementById("random");
+
+//loads initial page with preset color values from HTML and matching gradient background
+body.onload = function() {
+	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+	css.textContent = body.style.background + ";";
+	}
+//to set page background color based on colors picked from pallette
+function pickColor() {
+body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+css.textContent = body.style.background + ";";
+}
+//generates random color based on a string of 16 characters and using a for loop and math.random to string 6 new 
+//random characters together to form a hex code
+function randomColor(){
+	const letters = '0123456789ABCDEF';
+	let color = '#';
+	for(let i=0; i < 6; i++) {
+		color+= letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
+//sets variable values to return hex value from randomcolor and runs pickColor function to set background & Css 
+//text content
+function setRandomColors() {
+	color1.value = randomColor();
+	color2.value = randomColor();
+	pickColor();
 }
 
-//function to add strikethrough style when a list item is clicked
-function strikeThrough(event) {
-	if(event.target.tagName === "LI") {
-		event.target.classList.toggle("done");
-	}
-}
-function fireBrick(event) {
-	if(event.target.tagName === "H1") {
-		event.target.classList.toggle("coolTitle");
-	}
-}
 
-//checks input length
-function checkLength() {
-	return input.value.length;
-}
-//creates the additional list elements - appends a button child element with Delete label and instructs
-//button onclick to invoke removePrent function
-function createListElement() {
-		const but = document.createElement("button");
-		but.appendChild(document.createTextNode("Delete"));
-		but.onclick = removeParent;
-
-		const li = document.createElement("li");
-		li.appendChild(document.createTextNode(input.value));
-		list.appendChild(li);
-		li.appendChild(but);
-		input.value ="";
-}
-//removeParent function - removes parent from Nodelist along with eventlistener
-function removeParent(event) {
-	event.target.removeEventListener("click", removeParent, false);
-	event.target.parentNode.remove();
-}
-//invokes createlist element 
-function addAfterClick() {
-	if(checkLength() > 0){
-		createListElement();
-	}
-}
-//adds items by invoking createlistelement function
-function addOnEnter(event) {
-	if(checkLength() > 0 && event.which === 13) {
-		createListElement();
-	}
-}
-//changes header styling
-header.addEventListener("click", fireBrick);
-//event listener to invoke strikethrough style
-list.addEventListener("click", strikeThrough);
-//event listener for adding items via clicking enter
-button.addEventListener("click", addAfterClick);
-//event listener for adding items via enter button on keyboard
-input.addEventListener("keypress", addOnEnter);
+//event listeners to trigger changes based on user click/input
+random.addEventListener("click", setRandomColors);
+color1.addEventListener("input", pickColor);
+color2.addEventListener("input", pickColor);
